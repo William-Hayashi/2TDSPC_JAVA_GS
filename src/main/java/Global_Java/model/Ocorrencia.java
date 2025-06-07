cb6da1ddff3953de5efca7d51e3f017fac2caee7
William-Hayashi <william.hayashi@gmail.com>
2025-06-06 23:45:51 -0300

    Get e Set Ocorrencia

--- src/main/java/Global_Java/model/Ocorrencia.java
+++ src/main/java/Global_Java/model/Ocorrencia.java
@@ -1,8 +1,12 @@
 package Global_Java.model;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+@Setter
+@Getter
 @Document(collection = "Ocorrencia")
 public class Ocorrencia {
 
@@ -15,43 +19,4 @@ public class Ocorrencia {
 
     private Integer urgencia;
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getLocalizacao() {
-        return localizacao;
-    }
-
-    public void setLocalizacao(String localizacao) {
-        this.localizacao = localizacao;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public Integer getUrgencia() {
-        return urgencia;
-    }
-
-    public void setUrgencia(Integer urgencia) {
-        this.urgencia = urgencia;
-    }
 }
